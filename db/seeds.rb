2c737fc1e2fc027a94dc7749fef1af20caeeea46
Nhameo Benhebri <nhameo@gmail.com>
2016-12-01 17:02:23 +0100

    models ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,8 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.firs
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
